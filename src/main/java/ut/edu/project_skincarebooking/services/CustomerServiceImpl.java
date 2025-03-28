cd2205adb1074f2652bc5d40777af1d91958af45
Justhathanh <hvthanh1125@gmail.com>
2025-03-28 14:23:47 +0700

    API cua Customer

---
+++ src/main/java/ut/edu/project_skincarebooking/services/CustomerServiceImpl.java
@@ -0,0 +1,46 @@
+package ut.edu.project_skincarebooking.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ut.edu.project_skincarebooking.exceptions.CustomerNotFoundException;
+import ut.edu.project_skincarebooking.models.Customer;
+import ut.edu.project_skincarebooking.repositories.CustomerRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerServiceImpl implements CustomerService {
+    private final CustomerRepository customerRepository;
+
+    @Override
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Customer getCustomerById(Long id) {
+        return customerRepository.findById(id)
+                .orElseThrow(() -> new CustomerNotFoundException("Customer not found with ID: " + id));
+    }
+
+    @Override
+    public Customer createCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    @Override
+    public Customer updateCustomer(Long id, Customer updatedCustomer) {
+        Customer existingCustomer = getCustomerById(id);
+        existingCustomer.setName(updatedCustomer.getName());
+        existingCustomer.setEmail(updatedCustomer.getEmail());
+        existingCustomer.setPhone(updatedCustomer.getPhone());
+        return customerRepository.save(existingCustomer);
+    }
+
+    @Override
+    public void deleteCustomer(Long id) {
+        Customer existingCustomer = getCustomerById(id);
+        customerRepository.delete(existingCustomer);
+    }
+}
