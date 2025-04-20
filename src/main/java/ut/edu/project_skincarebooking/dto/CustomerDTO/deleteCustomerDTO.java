9df6668a2929e7defbfb6cb2be776a132cdd8ed3
Tra <tradtt6898@ut.edu.vn>
2025-04-20 20:29:43 +0700

    Cap nhat FE, chinh DTO cua chuyenvien
    # Conflicts:
    #	src/main/resources/application.properties

--- src/main/java/ut/edu/project_skincarebooking/dto/CustomerDTO/deleteCustomerDTO.java
+++ src/main/java/ut/edu/project_skincarebooking/dto/CustomerDTO/deleteCustomerDTO.java
@@ -1,14 +1,14 @@
-package ut.edu.project_skincarebooking.dto.CustomerDTO;
+    package ut.edu.project_skincarebooking.dto.CustomerDTO;
 
-public class deleteCustomerDTO {
-    private Long id;
+    public class deleteCustomerDTO {
+        private Long id;
 
-    public deleteCustomerDTO() {}
+        public deleteCustomerDTO() {}
 
-    public deleteCustomerDTO(Long id) {
-        this.id = id;
+        public deleteCustomerDTO(Long id) {
+            this.id = id;
+        }
+
+        public Long getId() { return id; }
+        public void setId(Long id) { this.id = id; }
     }
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-}
